517911d7348f486a60274514dab1e00f6757262d
nikola <nikola.antic@triplecrown.rs>
2018-05-21 13:27:57 +0200

    table

--- js/full-size.js
+++ js/full-size.js
@@ -7,7 +7,18 @@ function fullSize(tempthis) {
     let tempdataID = tempthis.dataset.id;
     for (let i = 0; i < tempclickedCity.length; i++) {
 
+
+
         if (tempdataID == destinationObject[i].id) {
+
+            let tempSeasons = Object.keys(destinationObject[i].seasons);
+
+            let tempSpring = Object.keys(destinationObject[i].seasons.spring);
+            let tempSummer = Object.keys(destinationObject[i].seasons.summer);
+            let tempAutumn = Object.keys(destinationObject[i].seasons.autumn);
+            let tempWinter = Object.keys(destinationObject[i].seasons.winter);
+
+
             let tempimageFullScreen = document.getElementById("imageFullScreen");
             let temph1FullScreenShow = document.getElementById("h1FullScreenShow");
             let temppFullScreen = document.getElementById("pFullScreen");
@@ -16,166 +27,82 @@ function fullSize(tempthis) {
             tempimageFullScreen.src = destinationObject[i].image;
             temppFullScreen.innerText = destinationObject[i].desc;
 
+            for (let q = 0; q < tempSeasons.length; q++) {
+                let temptablePrice = document.createElement("table");
 
-            let temptablePriceSpring = document.createElement("table");
-            temptablePriceSpring.id = "tablePriceSpring";
-            let temptablePriceTRSpring = document.createElement("tr");
-            temptablePriceTRSpring.innerText = "SPRING";
-            temptablePriceSpring.appendChild(temptablePriceTRSpring);
-            for (let k = 0; k < 2; k++) {
-                let temptablePriceTRSpring = document.createElement("tr");
-                let temptablePriceTDSpringMarch = document.createElement("td");
-                let temptablePriceTDSpringApril = document.createElement("td");
-                let temptablePriceTDSpringMay = document.createElement("td");
-                for (let j = 0; j < 3; j++) {
-                    let temptablePriceTRSpring = document.createElement("tr");
-                    if (k == 0) {
-                        temptablePriceTDSpringMarch.innerText = "March";
-                        temptablePriceTRSpring.appendChild(temptablePriceTDSpringMarch);
+                for (let w = 0; w < 2; w++) {
 
-                        temptablePriceTDSpringApril.innerText = "April";
-                        temptablePriceTRSpring.appendChild(temptablePriceTDSpringApril)
+                    let temptablePriceTR = document.createElement("tr");
 
-                        temptablePriceTDSpringMay.innerText = "May";
-                        temptablePriceTRSpring.appendChild(temptablePriceTDSpringMay);
+                    if (tempSeasons[q] == "spring") {
+                        for (let spr = 0; spr < tempSpring.length; spr++) {
+                            let temptablePriceTDSpring = document.createElement("td");
+
+                            if (w == 0) {
+                                temptablePriceTDSpring.innerText = tempSpring[spr];
+                            }
+                            else {
+                                temptablePriceTDSpring.innerText = destinationObject[i].seasons[tempSeasons[q]][tempSpring[spr]];
+                            }
+
+                            temptablePriceTR.appendChild(temptablePriceTDSpring);
+                        }
+                        temptablePrice.appendChild(temptablePriceTR);
                     }
-                    else {
 
-                        temptablePriceTDSpringMarch.innerText = destinationObject[i].seasons["spring"].March;
-                        temptablePriceTRSpring.appendChild(temptablePriceTDSpringMarch);
 
-                        temptablePriceTDSpringApril.innerText = destinationObject[i].seasons["spring"].April;
-                        temptablePriceTRSpring.appendChild(temptablePriceTDSpringApril);
+                    else if (tempSeasons[q] == "summer") {
+                        for (let sum = 0; sum < tempSummer.length; sum++) {
+                            let temptablePriceTDSummer = document.createElement("td");
 
-                        temptablePriceTDSpringMay.innerText = destinationObject[i].seasons["spring"].May;
-                        temptablePriceTRSpring.appendChild(temptablePriceTDSpringMay);
+                            if (w == 0) {
+                                temptablePriceTDSummer.innerText = tempSummer[sum];
+                            }
+                            else {
+                                temptablePriceTDSummer.innerText = destinationObject[i].seasons[tempSeasons[q]][tempSummer[sum]];
+                            }
 
+                            temptablePriceTR.appendChild(temptablePriceTDSummer);
+                        }
+                        temptablePrice.appendChild(temptablePriceTR);
+                    }
+
+                    else if (tempSeasons[q] == "autumn") {
+                        for (let aut = 0; aut < tempAutumn.length; aut++) {
+                            let temptablePriceTDAutumn = document.createElement("td");
+
+                            if (w == 0) {
+                                temptablePriceTDAutumn.innerText = tempAutumn[aut];
+                            }
+                            else {
+                                temptablePriceTDAutumn.innerText = destinationObject[i].seasons[tempSeasons[q]][tempAutumn[aut]];
+                            }
+
+                            temptablePriceTR.appendChild(temptablePriceTDAutumn);
+                        }
+                        temptablePrice.appendChild(temptablePriceTR);
+                    }
+
+                    else if (tempSeasons[q] == "winter") {
+                        for (let win = 0; win < tempWinter.length; win++) {
+                            let temptablePriceTDWinter = document.createElement("td");
+
+                            if (w == 0) {
+                                temptablePriceTDWinter.innerText = tempWinter[win];
+                            }
+                            else {
+                                temptablePriceTDWinter.innerText = destinationObject[i].seasons[tempSeasons[q]][tempWinter[win]];
+                            }
+
+                            temptablePriceTR.appendChild(temptablePriceTDWinter);
+                        }
+                        temptablePrice.appendChild(temptablePriceTR);
                     }
 
                 }
-                temptablePriceSpring.appendChild(temptablePriceTRSpring);
-
+                temptableDiv.appendChild(temptablePrice);
             }
-            temptableDiv.appendChild(temptablePriceSpring);
-
-            let temptablePriceSummer = document.createElement("table");
-            temptablePriceSummer.id = "tablePriceSummer";
-            let temptablePriceTRSummer = document.createElement("tr");
-            temptablePriceTRSummer.innerText = "SUMMER";
-            temptablePriceSummer.appendChild(temptablePriceTRSummer);
-            for (let k = 0; k < 2; k++) {
-                let temptablePriceTRSummer = document.createElement("tr");
-                let temptablePriceTDSummerJune = document.createElement("td");
-                let temptablePriceTDSummerJuly = document.createElement("td");
-                let temptablePriceTDSummerAugust = document.createElement("td");
-                for (let j = 0; j < 3; j++) {
-                    if (k == 0) {
-                        temptablePriceTDSummerJune.innerText = "June";
-                        temptablePriceTRSummer.appendChild(temptablePriceTDSummerJune);
-
-                        temptablePriceTDSummerJuly.innerText = "July";
-                        temptablePriceTRSummer.appendChild(temptablePriceTDSummerJuly)
-
-                        temptablePriceTDSummerAugust.innerText = "August";
-                        temptablePriceTRSummer.appendChild(temptablePriceTDSummerAugust);
-                    }
-                    else {
-
-                        temptablePriceTDSummerJune.innerText = destinationObject[i].seasons["summer"].June;
-                        temptablePriceTRSummer.appendChild(temptablePriceTDSummerJune);
-
-                        temptablePriceTDSummerJuly.innerText = destinationObject[i].seasons["summer"].July;
-                        temptablePriceTRSummer.appendChild(temptablePriceTDSummerJuly);
-
-                        temptablePriceTDSummerAugust.innerText = destinationObject[i].seasons["summer"].August;
-                        temptablePriceTRSummer.appendChild(temptablePriceTDSummerAugust);
-                    }
-                }
-                temptablePriceSummer.appendChild(temptablePriceTRSummer);
-            }
-            temptableDiv.appendChild(temptablePriceSummer);
-
-            let temptablePriceAutumn = document.createElement("table");
-            temptablePriceAutumn.id = "tablePriceAutumn";
-            let temptablePriceTRAutumn = document.createElement("tr");
-            temptablePriceTRAutumn.innerText = "AUTUMN";
-            temptablePriceAutumn.appendChild(temptablePriceTRAutumn);
-            for (let k = 0; k < 2; k++) {
-                let temptablePriceTRAutumn = document.createElement("tr");
-                let temptablePriceTDAutumnSeptember = document.createElement("td");
-                let temptablePriceTDAutumnOctober = document.createElement("td");
-                let temptablePriceTDAutumnNovember = document.createElement("td");
-                for (let j = 0; j < 3; j++) {
-                    if (k == 0) {
-                        temptablePriceTDAutumnSeptember.innerText = "September";
-                        temptablePriceTRAutumn.appendChild(temptablePriceTDAutumnSeptember);
-
-                        temptablePriceTDAutumnOctober.innerText = "October";
-                        temptablePriceTRAutumn.appendChild(temptablePriceTDAutumnOctober)
-
-                        temptablePriceTDAutumnNovember.innerText = "November";
-                        temptablePriceTRAutumn.appendChild(temptablePriceTDAutumnNovember);
-                    }
-                    else {
-                        temptablePriceTDAutumnSeptember.innerText = destinationObject[i].seasons["autumn"].September;
-                        temptablePriceTRAutumn.appendChild(temptablePriceTDAutumnSeptember);
-
-                        temptablePriceTDAutumnOctober.innerText = destinationObject[i].seasons["autumn"].October;
-                        temptablePriceTRAutumn.appendChild(temptablePriceTDAutumnOctober);
-
-                        temptablePriceTDAutumnNovember.innerText = destinationObject[i].seasons["autumn"].November;
-                        temptablePriceTRAutumn.appendChild(temptablePriceTDAutumnNovember);
-                    }
-                }
-                temptablePriceAutumn.appendChild(temptablePriceTRAutumn);
-            }
-            temptableDiv.appendChild(temptablePriceAutumn);
-
-            let temptablePriceWinter = document.createElement("table");
-            temptablePriceWinter.id = "tablePriceWinter";
-            let temptablePriceTRWinter = document.createElement("tr");
-            temptablePriceTRWinter.innerText = "WINTER";
-            temptablePriceWinter.appendChild(temptablePriceTRWinter);
-            for (let k = 0; k < 2; k++) {
-                let temptablePriceTRWinter = document.createElement("tr");
-                let temptablePriceTDWinterDecember = document.createElement("td");
-                let temptablePriceTDWinterJanuary = document.createElement("td");
-                let temptablePriceTDWinterFebruary = document.createElement("td");
-                for (let j = 0; j < 3; j++) {
-                    if (k == 0) {
-                        temptablePriceTDWinterDecember.innerText = "September";
-                        temptablePriceTRWinter.appendChild(temptablePriceTDWinterDecember);
-
-                        temptablePriceTDWinterJanuary.innerText = "October";
-                        temptablePriceTRWinter.appendChild(temptablePriceTDWinterJanuary)
-
-                        temptablePriceTDWinterFebruary.innerText = "November";
-                        temptablePriceTRWinter.appendChild(temptablePriceTDWinterFebruary);
-                    }
-                    else {
-                        temptablePriceTDWinterDecember.innerText = destinationObject[i].seasons["winter"].December;
-                        temptablePriceTRWinter.appendChild(temptablePriceTDWinterDecember);
-
-                        temptablePriceTDWinterJanuary.innerText = destinationObject[i].seasons["winter"].January;
-                        temptablePriceTRWinter.appendChild(temptablePriceTDWinterJanuary);
-
-                        temptablePriceTDWinterFebruary.innerText = destinationObject[i].seasons["winter"].February;
-                        temptablePriceTRWinter.appendChild(temptablePriceTDWinterFebruary);
-                    }
-                }
-                temptablePriceWinter.appendChild(temptablePriceTRWinter);
-            }
-            temptableDiv.appendChild(temptablePriceWinter);
-            tempmainSection.appendChild(tableDiv);
             tempmainSection.style.visibility = "visible";
-
         }
-
-
-
-
-
-
-
     }
 }
